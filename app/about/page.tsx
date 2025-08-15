c930180a6c6b098ed3ea21c5bb656f95125db30a
James Hughes <james.a.hughes@gmail.com>
2025-08-15 12:08:28 -0400

    metadata and SEO

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,14 @@
-"use client";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About Us | Name of Website",
+  description: "A lot of keywords",
+  keywords: "about, company, mission, ai, saas",
+  twitter: {
+    card: "summary_large_image",
+    title: "About Us | Twitter",
+  },
+};
 
 const About = () => {
   console.log("is this in the server or client?");
